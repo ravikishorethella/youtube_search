fbc0e89afc4cd22db00b91476c629b7262e3ee85
ravikishorethella <ravikishorethella@gmail.com>
2018-06-06 10:56:44 -0400

    ...

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const API_KEY = "AIzaSyCCAshZmKO5ohMvNJDoabv4ZgD-wBXreFI";
+
+const App = () => {
+  return <div>Hello</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector(".container"));
