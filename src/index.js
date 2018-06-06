7a0be253da562dac0ce859b973ac6609a7b4b3bc
ravikishorethella <ravikishorethella@gmail.com>
2018-06-06 15:37:22 -0400

    almost complete

--- src/index.js
+++ src/index.js
@@ -3,23 +3,31 @@ import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
+
+// const API_KEY = // Use your own api;
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      videos: []
+      videos: [],
+      selectedVideo: null
     };
     YTSearch({ key: API_KEY, term: "javascript" }, data => {
-      this.setState({ videos: data });
+      this.setState({ videos: data, selectedVideo: data[0] });
     });
   }
   render() {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+        />
       </div>
     );
   }
