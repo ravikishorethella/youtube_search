9a76678c218afee520a8526f217967876e9806e4
ravikishorethella <ravikishorethella@gmail.com>
2018-06-06 15:02:58 -0400

    ,,,,,

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,10 +4,17 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { text: "" };
+    this.state = { term: "" };
   }
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+      </div>
+    );
   }
 }
 
